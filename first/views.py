99c93fb1903c9c4b46780dfe893cdf78cf459645
mariByr <byramarichka@gmail.com>
2026-05-07 19:32:37 +0300

    lesson1 first app

---
+++ first/views.py
@@ -0,0 +1,28 @@
+
+
+# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class FirstView(APIView):
+    def get(self, *args, **kwargs):
+        print(self.request.query_params.dict())
+       
+    
+    def post(self, *args, **kwargs):
+        print(self.request.data)
+        return Response("Hello from post")
+
+    def put(self, *args, **kwargs):
+        return Response("Hello from put")
+
+    def patch(self, *args, **kwargs):
+        return Response("Hello from patch")
+
+    def delete(self, *args, **kwargs):
+        return Response("Hello from delete")
+
+class SecondView(APIView):
+    def get(self, *args, **kwargs):
+        print(kwargs)
+        return Response(kwargs["age"])
